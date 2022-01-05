78d4c7a71bc713d93945ea0384665a3ae2646de4
DanielStoi <mrbunton29@gmail.com>
2022-01-05 23:33:06 +1100

    tried to pause music after play is pressed to avoid the repeating glichy sound on html

--- Assets/Music/BackgroundMusic.gd
+++ Assets/Music/BackgroundMusic.gd
@@ -6,10 +6,9 @@ export (Array, AudioStreamOGGVorbis) var menu_music : Array
 func play_menu_music():
 	$"/root/BackgroundMusic".stream = menu_music[0]
 	$"/root/BackgroundMusic".play(1.8)
-	$"/root/BackgroundMusic".pause_mode = false
-	pass
+	$"/root/BackgroundMusic".stream_paused = false
 
 #just to avoid the annoying repeating sounds on the html version
 func pause_music():
-	$"/root/BackgroundMusic".pause_mode = true
+	$"/root/BackgroundMusic".stream_paused = true
 	
